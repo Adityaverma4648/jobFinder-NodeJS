e0a34080eb2e7143747674da725a740f3f126a41
Aditya verma <adityaverma4648@gmail.com>
2023-03-04 22:32:24 +0530

    added handlebars

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 // express instance
 const app = express();
 // DB connect 
@@ -7,11 +9,22 @@ const connectDB = require("./config/connect");
 // PORT decision
 const PORT = process.env.PORT||5000;
 
+//  handle bars engine --------------------------------------------------------------------------------
+app.engine('hbs',exphbs({
+    defaultLayout : 'main',
+    extname : '.hbs'
+}));
+
+app.set('view engine','hbs');
+
+//  block ends here-----------------------------------------------------------------------------
+
+
 app.get("/",(req,res)=>{
     res.send("Hello from backend")
 })
 
-app.use('/api',apiRoutes);
+// app.use('/api',apiRoutes);
 
 
 
